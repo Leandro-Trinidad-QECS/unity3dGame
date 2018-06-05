276c4dda935a6f32b9b5ea80fef0b8f8b12c98a8
Leandro <l.trinidad@share.epsb.ca>
2018-06-05 06:57:33 -0600

    update files

---
+++ Run2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 7.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    public Animator anim;
+    public Collider coll;
+	private void Start()
+	{
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider>();
+	}
+	void Update()
+    {
+            
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+
+
+            
+            moveDirection = new Vector2(Input.GetAxis("Horizontal"), 0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+                
+
+        } else {
+            moveDirection = new Vector2(Input.GetAxis("Horizontal"),moveDirection.y);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection.x *= speed / 2;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
